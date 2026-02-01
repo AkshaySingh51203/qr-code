2aa640c1f0fc09e053f28d17220ace6a768b3acf
AkshaySingh51203 <asingh51203@ucumberlands.edu>
2026-02-01 23:36:10 +0530

    Update asingh_qr.py

--- asingh_qr.py
+++ asingh_qr.py
@@ -1,13 +1,13 @@
 import qrcode
 
-# URL you want to open when QR is scanned
-data = "https://github.com/dashboard"
+# GitHub repository URL
+data = "https://github.com/AkshaySingh51203/qr-code"
 
 # Generate QR code
 img = qrcode.make(data)
 
-# Save QR code to a fixed location (easy to find)
-img.save(r"D:\github_dashboard_qr.png")
+# Save QR code image to D drive
+img.save(r"D:\github_repo_qr.png")
 
 print("QR Code generated successfully!")
-print("File saved at D:\\github_dashboard_qr.png")
+print("File saved at D:\\github_repo_qr.png")
